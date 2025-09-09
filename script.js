00909c1431604e958648885cd3c9687fd1371d8b
Sajjadpp <sajjadmuhammed227@gmail.com>
2025-09-09 13:25:04 +0530

    music corrected

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let name = 'Arshi'
-
 const nameSpan = document.querySelector('#name-span');
-
-console.log(nameSpan)
 nameSpan.innerHTML = name
+
+
+
+document.addEventListener("click", function () {
+    const audio = document.getElementById("bg-music");
+    audio.muted = false;
+    audio.play();
+});
